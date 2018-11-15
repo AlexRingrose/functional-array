5aeaabeb614f5590188018584a17c981a33a0ee8
AlexRingrose <43159722+AlexRingrose@users.noreply.github.com>
2018-11-15 00:35:15 +0000

    problem 1

--- arrayFilter.js
+++ arrayFilter.js
@@ -1,8 +1,15 @@
-const Animals = [    
-	{name: “Fluffykins” , species: “rabbit” },
-	{name: “Caro” , species: “dog” },
-	{name: “Hamilton” , species: “dog”},
-	{name: “Harold”, species: “fish”},
-	{name: “Ursula” , species: “cat”},
-	{name: “Jimmy”, species: “fish”}
+const Animals = [
+	{name:"Fluffykins",species:"rabbit"},
+	{name: "Caro" , species: "dog" },
+	{name: "Hamilton" , species: "dog"},
+	{name: "Harold", species: "fish"},
+	{name: "Ursula" , species: "cat"},
+	{name: "Jimmy", species: "fish"}
 ];
+
+const arrayFilter = function(array){
+    let dogs = array.filter(word => word.species === "dog");
+    console.log(dogs);
+};
+
+arrayFilter(Animals);
